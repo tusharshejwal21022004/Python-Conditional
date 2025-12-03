4007342566180edcd58d682d6eeb64c32ce04436
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-03 20:46:12 +0530

    Add files via upload

---
+++ 0-intro.py
@@ -0,0 +1,10 @@
+#Conditional Statement:
+
+#Basically it is used to contral the flow of program
+
+#A block of code which executed under certain conditiom
+
+# There are three types of conditional statement:
+# 1. If
+# 2. If else
+# 3. If elif else
